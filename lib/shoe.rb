c3c08a853f0708ef82401ff8d5c32157b2e8c3a0
Andrea Tchalaou <attchalaou@gmail.com>
2020-07-13 07:28:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,8 @@ class Shoe
 
   def initialize(brand)
      @brand = brand
-    if BRANDS.include?(brand)
-    end
+    # if BRANDS.include?(brand)
+    # end
   # else !brand.include?(BRAND) << brand
   end
    
